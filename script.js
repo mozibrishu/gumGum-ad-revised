d54db78008f88ec4a4ecd4db3acd4bbc91906482
mozibrishu <mozib.diu@gmail.com>
2022-09-13 06:49:30 +0600

    first page done

--- script.js
+++ script.js
@@ -10,15 +10,24 @@ document.querySelector('.containerBox').addEventListener('mouseover', ()=>{
 
     document.querySelector('.containerBox').classList.add('newContainerBox');
     document.querySelector('.videoContainer').classList.add('newVideoContainer');
+    document.querySelector('.buynow').classList.add('buynow-move');
+    document.querySelector('.Onegb').classList.add('Onegb-move');
 
+    document.querySelector('.imageText').style.opacity = 0;
 
+    document.querySelector('.imageTextMove').classList.add('imageTextMove-animation');
+    document.querySelector('.imageTextMove').style.opacity = 1;
 });
 
 document.querySelector('.containerBox').addEventListener('mouseout', ()=>{
     document.querySelector('.imageTextContainer').classList.remove('newImageTextContainer');
     document.querySelector('.containerBox').classList.remove('newContainerBox');
     document.querySelector('.videoContainer').classList.remove('newVideoContainer');
-
+    document.querySelector('.buynow').classList.remove('buynow-move');
+    document.querySelector('.Onegb').classList.remove('Onegb-move');
+    document.querySelector('.imageText').style.opacity = 1;
+    document.querySelector('.imageTextMove').style.opacity = 0;
+    document.querySelector('.imageTextMove').classList.remove('imageTextMove-animation');
 });
 
 
