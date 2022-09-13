b985779e3a78b6d2afb47770948b514bf5223e2c
mozibrishu <mozib.diu@gmail.com>
2022-09-13 07:01:43 +0600

    Video control added

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let containerBox = document.querySelector('#containerBox');
 let flexContainer = document.querySelector('#flexContainer');
 let videoContainer = document.querySelector('#videoContainer');
 let imageTextContainer = document.querySelector('#imageTextContainer');
-
+let mute =1;
 
 document.querySelector('.containerBox').addEventListener('mouseover', ()=>{
 
@@ -31,3 +31,59 @@ document.querySelector('.containerBox').addEventListener('mouseout', ()=>{
 });
 
 
+function playMute() {
+    var player = document.querySelector('#videoContainer');
+    var video = player.querySelector('.myVideo');
+    var toggle = player.querySelector('#toggle');
+    var sound = player.querySelector('#sound');
+    
+    var playIcon = `<i class="material-icons">play_arrow</i>`;
+    var pauseIcon = `<i class="material-icons">pause</i>`;
+    var volumeUp = `<i class="material-icons">volume_up</i>`;
+    var volumeOff = `<i class="material-icons">volume_off</i>`;
+    var replayIcon = `<i class="material-icons">replay</i>`;
+    if(!mute){
+        video.muted = false;
+        sound.innerHTML = volumeUp;
+    }else{
+        video.muted = true;
+        sound.innerHTML = volumeOff;
+    }
+    // console.log(video.paused);
+    toggle.innerHTML = video.paused ? playIcon : pauseIcon; 
+    function togglePlay(e) {
+        e.stopPropagation();
+        var playOrPause = video.paused ? 'play' : 'pause';
+        video[playOrPause]();
+    }
+    
+    function updatePlayPause(e) {
+        e.stopPropagation();
+        var playPauseIcon = this.paused ? playIcon : pauseIcon;    
+        toggle.innerHTML = playPauseIcon;
+    }
+    
+    function toggleSound(e) {
+        e.stopPropagation();
+        if(video.muted){
+            video.muted = false;
+        sound.innerHTML = volumeUp;
+        mute=0;
+        }else{
+            video.muted = true;
+        sound.innerHTML = volumeOff;
+        mute=1;
+        }
+        // var soundIcon = video.muted ? volumeUp : volumeOff;
+        // sound.innerHTML = soundIcon;
+    
+        // if (video.muted) video.muted = false;
+        // else video.muted = true;
+    }
+    
+    video.addEventListener('play', updatePlayPause);
+    video.addEventListener('pause', updatePlayPause);
+    toggle.addEventListener('click', togglePlay);
+    sound.addEventListener('click', toggleSound);
+        
+}
